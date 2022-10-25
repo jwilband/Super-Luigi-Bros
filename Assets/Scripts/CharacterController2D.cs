e0b9b06d64e9f4d1b8aab7cbc337495d42a3a0bb
Jesse <jesse.wilband@outlook.com>
2022-10-25 07:35:14 -0400

    Create prefabs

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public float maxWalkSpeed = 2.0f;
+    public float maxRunSpeed = 5.0f;
+    public float maxJump = 6.0f;
+
+    bool facingRight = true;
+    bool grounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+      
+    }
+}
